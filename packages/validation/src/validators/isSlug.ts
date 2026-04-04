20db871053a860b4bd9ed1e8b6d33a810df6d691
Guillermo <jguillermo@outlook.com>
2026-04-04 04:13:54 -0500

    add validation library

--- packages/validation/src/validators/isSlug.ts
+++
@@ -1,8 +0,0 @@
-import assertString from './util/assertString';
-
-const charsetRegex = /^[a-z0-9](?!.*[-_]{2,})(?:[a-z0-9_-]*[a-z0-9])?$/;
-
-export default function isSlug(str) {
-  assertString(str);
-  return charsetRegex.test(str);
-}
